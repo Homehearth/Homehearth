8a8246f34e8035aaabee150033c6e6f8a39537d6
Jonathan Helsing <jonathanhelsing.source@gmail.com>
2021-09-03 09:02:10 +0200

    premake documentation.

--- Game/premake5.lua
+++ Game/premake5.lua
@@ -7,15 +7,21 @@ project "Game"
     targetdir("build/bin/" .. outputdir .. "/%{prj.name}")
     objdir("build/bin-int/" .. outputdir .. "/%{prj.name}")
 
+
+    -- Define the location(s) of files.
     files {
         "src/**.h",
 		"src/**.cpp",
     }
 
+
+    -- Define any includes of folder(s).
+    -- Useful to remove any "../folder/file.h" in code.
     includedirs {
         "src"
     }
 
+    
     filter {"system:windows"}
         defines {
         
