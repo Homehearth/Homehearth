90a86ab67fd16c382300916d83530268f82346b1
klanton <rikard.lundberg@hotmail.com>
2021-09-06 16:04:52 +0200

    added ifdefs for debug.

--- ThirdParty/networking/net_client_interface.h
+++ ThirdParty/networking/net_client_interface.h
@@ -82,7 +82,9 @@ namespace network
 
 		if (rv != 0)
 		{
+			#ifdef _DEBUG
 			std::cout << "WSAStartup error code: " << WSAGetLastError() << std::endl;
+			#endif
 
 			return false;
 		}
@@ -91,7 +93,9 @@ namespace network
 		{
 			/* Tell the user that we could not find a usable */
 			/* WinSock DLL.                                  */
-			std::cerr << "Could not find a usable version of Winsock.dll" << std::endl;
+			#ifdef _DEBUG
+			std::cout << "Could not find a usable version of Winsock.dll" << std::endl;
+			#endif
 
 			return false;
 		}
@@ -102,6 +106,10 @@ namespace network
 		ZeroMemory(&hints, sizeof(hints));
 		hints.ai_family = AF_INET;
 		hints.ai_socktype = SOCK_STREAM;
+		
+		#ifdef _DEBUG
+		std::cout << "Creating a client.." << std::endl;
+		#endif
 
 		rv = getaddrinfo(ip.c_str(), std::to_string(port).c_str(), &hints, &servinfo);
 
@@ -114,7 +122,9 @@ namespace network
 		// Loop through linked list of possible network structures
 		for (p = servinfo; p != nullptr; p = p->ai_next)
 		{
+			#ifdef _DEBUG
 			std::cout << PrintSocketData(p) << std::endl;
+			#endif
 			m_socket = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
 
 			if (m_socket == INVALID_SOCKET)
@@ -123,7 +133,9 @@ namespace network
 			}
 			if (connect(m_socket, p->ai_addr, static_cast<int>(p->ai_addrlen)) != 0)
 			{
-				std::cerr << "Connect error code: " << WSAGetLastError() << std::endl;
+				#ifdef _DEBUG
+				std::cout << "Connect error code: " << WSAGetLastError() << std::endl;
+				#endif
 				m_socket = INVALID_SOCKET;
 				closesocket(m_socket);
 				continue;
@@ -134,7 +146,9 @@ namespace network
 		// Reached end of list and could not connect to any
 		if (p == nullptr)
 		{
-			std::cerr << "Failed to connect!" << std::endl;
+			#ifdef _DEBUG
+			std::cout << "Failed to connect!" << std::endl;
+			#endif
 			m_socket = INVALID_SOCKET;
 
 			return false;
@@ -153,7 +167,9 @@ namespace network
 
 		if (closesocket(m_socket) != 0)
 		{
-			std::cerr << "Failed to close socket: " << WSAGetLastError() << std::endl;
+			#ifdef _DEBUG
+			std::cout << "Failed to close socket: " << WSAGetLastError() << std::endl;
+			#endif
 		}
 
 		m_socket = INVALID_SOCKET;
