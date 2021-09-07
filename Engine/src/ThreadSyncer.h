006a388a1fe8611a9c04ae9ea6f84021253f34d5
DAVID WINGQVIST <daag19@student.bth.se>
2021-09-07 16:18:16 +0200

    RTexture and ThreadSync work.

--- Engine/src/ThreadSyncer.h
+++ Engine/src/ThreadSyncer.h
@@ -1,28 +1,40 @@
 #pragma once
 
-class ThreadSyncer
+namespace thread
 {
-private:
+	class ThreadSyncer
+	{
+	private:
 
-	static ThreadSyncer* instance;
-	ThreadSyncer();
-	~ThreadSyncer();
+		static ThreadSyncer* instance;
+		ThreadSyncer();
+		~ThreadSyncer();
 
-public:
+	public:
 
-	// Used for safe exit during program closure.
-	static bool s_engineRunning;
-	static bool s_drawRunning;
-	static bool s_networkRunning;
+		// Used for safe exit during program closure.
+		static bool s_engineRunning;
+		static bool s_drawRunning;
+		static bool s_networkRunning;
 
-	// Used for sync between threads to prevent read/write on same block.
-	static unsigned int s_engineIndex;
-	static unsigned int s_drawIndex;
-	static unsigned int s_networkIndex;
+		// Used for sync between threads to prevent read/write on same block.
+		static int s_engineIndex;
+		static int s_drawIndex;
+		static int s_networkIndex;
 
-public:
+	public:
 
-	static void Initialize();
-	static void Destroy();
+		static void Initialize();
+		static void Destroy();
 
-};
+	};
+}
+
+// Engine Index
+#define eDex thread::ThreadSyncer::instance->s_engineIndex
+
+// Draw Index
+#define dDex thread::ThreadSyncer::instance->s_drawIndex
+
+// Network Index
+#define nDex thread::ThreadSyncer::instance->s_networkIndex
