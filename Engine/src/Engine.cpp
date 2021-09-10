2462a945ac9317af48936633185c7925efc817f2
klanton <rikard.lundberg@hotmail.com>
2021-09-10 10:38:35 +0200

    Merged again

--- Engine/src/Engine.cpp
+++ Engine/src/Engine.cpp
@@ -41,8 +41,7 @@ void Engine::Setup(const HINSTANCE& hInstance) {
     // Thread should be launched after s_engineRunning is set to true and D3D11 is initalized.
     s_engineRunning = true;
 
-    // register Engine OnEvent function so Scene can talk to Engine
-    Scene::GetEventDispatcher().sink<EngineEvent>().connect<&Engine::OnEvent>(this);
+    m_client = std::make_unique<Client>();
 
 }
 
@@ -52,15 +51,6 @@ void Engine::Start()
 
     if (thread::IsThreadActive())
         T_CJOB(Engine, RenderThread);
-
-    m_client = std::make_unique<Client>();
-    if (m_client->Connect("127.0.0.1", 4950))
-    {
-        if (thread::IsThreadActive())
-        {
-            thread::MultiThreader::InsertJob(std::bind([this] { m_client->Update(); }));
-        }
-    }
 
     MSG msg = { nullptr };
     while (IsRunning())
@@ -95,9 +85,9 @@ void Engine::Shutdown()
 {
     s_engineRunning = false;
     // Wait for the rendering thread to exit its last render cycle and shutdown.
-    while (!s_safeExit && !s_networkSafeExit) {};
+    while (!s_safeExit) {};
 
-    thread::MultiThreader::Destroy();
+    T_DESTROY();
     resource::ResourceManager::Destroy();
     D2D1Core::Destroy();
 }
