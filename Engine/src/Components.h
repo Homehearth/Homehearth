a4c40503fae548fc163f11ee3b801480e8da4a37
Julius Wendt <julius.wendt@live.se>
2021-09-16 15:40:14 +0200

    put components in ecs namespace

--- Engine/src/Components.h
+++ Engine/src/Components.h
@@ -1,12 +1,19 @@
 #pragma once
 #include "EnginePCH.h"
 
-struct Transform {
-	sm::Vector3 position;
-	sm::Vector3 rotation;
-	sm::Vector3 scale;
-};
+namespace ecs 
+{
 
-struct Model {
+	struct Transform 
+	{
+		sm::Vector3 position;
+		sm::Vector3 rotation;
+		sm::Vector3 scale;
+	};
 
-};
+	struct Model 
+	{
+
+	};
+
+}
