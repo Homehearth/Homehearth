24eef228dc5e4f2103de4af295b531050d668d85
Janai Gonzalez <jani_99_@hotmail.com>
2021-09-24 16:33:33 +0200

    removed text that came from merge

--- Game/src/Game.cpp
+++ Game/src/Game.cpp
@@ -14,7 +14,6 @@ Game::Game()
 	// Create or get scene
 	Scene& demo = m_engine.GetScene("Demo");
 	// Scene logic
-<<<<<<< HEAD
 	//setupDemoScene(m_engine, demo);
 	for (int i = 0; i < 1000; i++) {
 		auto e = demo.GetRegistry().create();
@@ -40,9 +39,8 @@ Game::Game()
 				});
 			
 		});
-=======
 	setupDemoScene(m_engine, demo);
->>>>>>> master
+
 	
 	//Set as current scene
 	m_engine.SetScene(demo);
