708e59d7fd100faf4780be0c24bb0788a55474dc
Janai Gonzalez <jani_99_@hotmail.com>
2021-10-01 09:24:13 +0200

    Fixed camera to work with realtive instead of absolut mouse position

--- Engine/src/Engine.cpp
+++ Engine/src/Engine.cpp
@@ -35,7 +35,8 @@ void Engine::Startup()
 	D2D1Core::Initialize(&m_window);
 
 	//Camera
-	m_debugCamera.Initialize(sm::Vector3(0, 0, -1), sm::Vector3(0, 0, 0), sm::Vector3(0, 1, 0), sm::Vector2((float)m_window.GetWidth(), (float)m_window.GetHeight()));
+	Camera m_debugCamera;
+	m_debugCamera.Initialize(sm::Vector3(0, 0, 1), sm::Vector3(0, 0, 0), sm::Vector3(0, 1, 0), sm::Vector2((float)m_window.GetWidth(), (float)m_window.GetHeight()));
 
 	m_currentCamera = std::make_shared<Camera>(m_debugCamera);
 
@@ -98,7 +99,6 @@ void Engine::Run()
 	while (IsRunning())
 	{
 		PROFILE_SCOPE("Frame");
-		InputSystem::Get().UpdateEvents();
 
 		while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
 		{
@@ -137,7 +137,6 @@ void Engine::Run()
 		// Handle Input.
 		InputSystem::Get().UpdateEvents();
 		
-		m_debugCamera.Update(deltaTime);
 		
 		//Showing examples of keyboard and mouse (THIS CODE SHOULD BE HANDLED SOMEWHERE ELSE (GAMEPLAY LOGIC))
 		if (InputSystem::Get().CheckKeyboardKey(dx::Keyboard::G, KeyState::RELEASED))
@@ -396,6 +395,8 @@ void Engine::Update(float dt)
 	if (m_currentScene)
 	{
 		m_currentScene->Update(dt);
+		m_currentCamera->Update(dt);
+
 	}
 
 	{
