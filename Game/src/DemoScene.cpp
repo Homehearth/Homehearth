840774d3d09d0db74be7c1758511372ab37e3d1c
FilipWickstrom <fiai18@student.bth.se>
2021-10-05 17:16:17 +0200

    Added new models

--- Game/src/DemoScene.cpp
+++ Game/src/DemoScene.cpp
@@ -9,7 +9,7 @@ void InitializePlayerEntity(Scene& scene)
 	auto& renderable = scene.GetRegistry().emplace<comp::Renderable>(playerEntity);
 	auto& player = scene.GetRegistry().emplace<comp::Player>(playerEntity);
 	player.runSpeed = 10.f;
-	renderable.mesh = ResourceManager::Get().GetResource<RMesh>("Tree1.obj");
+	renderable.mesh = ResourceManager::Get().GetResource<RMesh>("StreetLamp.obj");
 }
 
 void setupDemoScene(Scene& scene, Client& client)
