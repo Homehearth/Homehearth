57d431b73bb8138564c00cc1d99b9de2bbd45619
Jonathan Helsing <jonathanhelsing.source@gmail.com>
2021-10-07 01:14:08 +0200

    .

--- Engine/src/BasePass.cpp
+++ Engine/src/BasePass.cpp
@@ -6,15 +6,14 @@
 
 void BasePass::Initialize()
 {
+	// Initialize Pass.
+	// accept scene.
+	// accept materials.
+	// accept ...
 }
 
-void BasePass::PreRender()
+void BasePass::PreRender(ID3D11DeviceContext* dc, PipelineManager* pm)
 {
-    ClearPipelineSettings();
-	
-    auto dc = GetContext();
-    auto pm = GetPipelineManager();
-	
     // INPUT ASSEMBLY.
     {
         dc->IASetInputLayout(pm->m_defaultInputLayout.Get());
@@ -25,18 +24,24 @@ void BasePass::PreRender()
     {
         dc->VSSetShader(pm->m_defaultVertexShader.Get(), nullptr, 0);
         dc->PSSetShader(pm->m_defaultPixelShader.Get(), nullptr, 0);
+        dc->GSSetShader(nullptr, nullptr, 0);
+        dc->HSSetShader(nullptr, nullptr, 0);
+        dc->DSSetShader(nullptr, nullptr, 0);
+        dc->CSSetShader(nullptr, nullptr, 0);
     }
 
     // CONSTANT BUFFERS.
     {
-        dc->VSSetConstantBuffers(1, 1, GetCamera()->m_viewConstantBuffer.GetAddressOf());
+        dc->PSSetConstantBuffers(0, 0, nullptr);
+        dc->VSSetConstantBuffers(0, 1, pm->m_defaultModelConstantBuffer.GetAddressOf());
+        dc->VSSetConstantBuffers(1, 1, m_camera->m_viewConstantBuffer.GetAddressOf());
     }
 
     // SHADER RESOURCES.
     {
-        dc->PSSetShaderResources(10, 1, pm->m_depthBufferSRV.GetAddressOf());	// DepthBuffer.
+        dc->PSSetShaderResources(0, 0, nullptr);
+        dc->VSSetShaderResources(0, 0, nullptr);
         dc->PSSetSamplers(0, 1, pm->m_linearSamplerState.GetAddressOf());
-        dc->PSSetSamplers(1, 1, pm->m_pointSamplerState.GetAddressOf());
     }
 
     // RASTERIZER.
@@ -47,31 +52,25 @@ void BasePass::PreRender()
 
     // OUTPUT MERGER.
     {
-        dc->OMSetRenderTargets(1, pm->m_backBufferTarget.GetAddressOf(), nullptr);
-        dc->OMSetDepthStencilState(pm->m_lessDSS.Get(), 0);
+        dc->OMSetRenderTargets(1, pm->m_renderTargetView.GetAddressOf(), pm->m_depthStencilView.Get());
         dc->OMSetBlendState(pm->m_blendStatepOpaque.Get(), nullptr, 0xFFFFFFFF); 
+        dc->OMSetDepthStencilState(pm->m_depthStencilStateLess.Get(), 1);
     }
 }
 
-void BasePass::Render()
+void BasePass::Render(Scene* pScene)
 {
-    ID3D11Buffer* buffers[] = { GetScene()->GetRenderableBuffer().GetBuffer() };
-
-    // System that renders Renderable component.
-    GetContext()->VSSetConstantBuffers(0, 1, buffers);
-
-    auto& renderableCopies = GetScene()->GetRenderableCopies();
-    for (auto& it : renderableCopies[1])
-    {
-        GetScene()->GetRenderableBuffer().SetData(D3D11Core::Get().DeviceContext(), it.data);
-        it.mesh->Render();
-    }
-
-    renderableCopies.ReadyForSwap();
+	// Render objects.
+    pScene->Render();
 }
 
 void BasePass::PostRender()
 {
+	// return rendertarget for next pass?
+    D3D11Core::Get().DeviceContext()->UpdateSubresource(m_camera->m_viewConstantBuffer.Get(), 0, nullptr, m_camera->GetCameraMatrixes(), 0, 0);
+}
 
-    
+void BasePass::GetCamera(Camera* camera)
+{
+    m_camera = camera;
 }
