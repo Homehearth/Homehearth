923e9f82b08cbec5ea062eb7eec39e63cdef0a4d
MARTIN HÖGLUND <mahh18@student.bth.se>
2021-10-07 14:21:21 +0200

    Added comment

--- Engine/src/Model_ps.hlsl
+++ Engine/src/Model_ps.hlsl
@@ -75,7 +75,7 @@ float4 main(PixelIn input) : SV_TARGET
     //If an object has a texture sample from it, else use default values.
     if(c_hasAlbedo == 1)
     {
-        albedo = pow(T_albedo.Sample(samp, input.uv).rgb, 2.2f);
+        albedo = pow(T_albedo.Sample(samp, input.uv).rgb, 2.2f); //Power the albedo by 2.2f to get it to linear space.
         
     }
     
