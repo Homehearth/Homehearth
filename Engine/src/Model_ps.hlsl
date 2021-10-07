9aa9d7a1a61684ca338fa27b273de86e7be072bb
MARTIN HÖGLUND <mahh18@student.bth.se>
2021-10-07 14:17:54 +0200

    Albedo Color is now calculated correctly

--- Engine/src/Model_ps.hlsl
+++ Engine/src/Model_ps.hlsl
@@ -75,7 +75,8 @@ float4 main(PixelIn input) : SV_TARGET
     //If an object has a texture sample from it, else use default values.
     if(c_hasAlbedo == 1)
     {
-        albedo = T_albedo.Sample(samp, input.uv).rgb;
+        albedo = pow(T_albedo.Sample(samp, input.uv).rgb, 2.2f);
+        
     }
     
     if(c_hasNormal == 1)
