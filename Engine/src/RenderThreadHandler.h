94777c68a1733158b11424fc7c4f85994741a4ff
DAVID WINGQVIST <daag19@student.bth.se>
2021-10-13 16:44:08 +0200

    temp fix threads

--- Engine/src/RenderThreadHandler.h
+++ Engine/src/RenderThreadHandler.h
@@ -14,7 +14,7 @@ namespace thread
 	The must-meet threshold for divided rendering.
 		.Per thread object render.
 	*/
-	const int THRESHOLD = 1;
+	const int THRESHOLD = 999999;
 
 	class RenderThreadHandler
 	{
