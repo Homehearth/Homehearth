1038c4a5a19ed8f700e982c929f65918d739810d
Julius Wendt <julius.wendt@live.se>
2021-10-27 15:48:47 +0200

    rotation working better

--- NetServer/src/ServerSystems.cpp
+++ NetServer/src/ServerSystems.cpp
@@ -4,6 +4,8 @@
 namespace systems {
 	void CharacterMovement(HeadlessScene& scene, float dt)
 	{
+		
+
 		scene.ForEachComponent<comp::Player, comp::Transform, comp::Velocity>([&](comp::Player& p, comp::Transform& t, comp::Velocity& v)
 			{
 				if (v.vel.Length() > 0.001f)
@@ -13,7 +15,15 @@ namespace systems {
 
 					float time = dt * p.runSpeed;
 					float targetRotation = atan2(-vel.z, vel.x);
+
+					float deltaRotation = targetRotation - t.rotation.y;
+					
+					if (std::abs(deltaRotation) > dx::g_XMPi[0])
+					{
+						t.rotation.y += (t.rotation.y < 0.0f) ? dx::g_XMTwoPi[0] : -dx::g_XMTwoPi[0];
+					}
 					t.rotation.y = t.rotation.y * (1 - time) + targetRotation * time;
+
 				}
 			});
 	}
