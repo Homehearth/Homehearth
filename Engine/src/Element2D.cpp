76f2511ce3f1ec84e936b5a18cea42ed7514b6b2
klanton <rikard.lundberg@hotmail.com>
2021-10-29 09:45:28 +0200

    Small bug fix from element2D

--- Engine/src/Element2D.cpp
+++ Engine/src/Element2D.cpp
@@ -1,6 +1,9 @@
 #include "EnginePCH.h"
 
 bool Element2D::IsHovered() const
+{
+	return m_isHovering;
+}
 bool Element2D::IsVisible() const
 {
 	return m_isVisible;
@@ -9,11 +12,6 @@ bool Element2D::IsVisible() const
 void Element2D::SetVisiblity(const bool& toggle)
 {
 	m_isVisible = toggle;
-}
-
-const bool Element2D::IsHovered() const
-{
-	return m_isHovering;
 }
 
 void Element2D::AddRef()
