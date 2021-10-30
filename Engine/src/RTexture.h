b74760628b8c5b0aea6c2bf50344c565f01fa8dd
FilipWickstrom <fiai18@student.bth.se>
2021-10-30 12:48:56 +0200

    Possible to render with mipmapping on the textures

--- Engine/src/RTexture.h
+++ Engine/src/RTexture.h
@@ -2,6 +2,11 @@
 #include "GResource.h"
 const UINT MAXSIZE = 4096;
 
+/*
+	Can genarate mipmaps for textures to avoid flickering on textures far away.
+	Takes 1/3 extra memory per texture.
+*/
+
 enum class ETextureChannelType
 {
 	oneChannel,
@@ -11,14 +16,17 @@ enum class ETextureChannelType
 class RTexture : public resource::GResource
 {
 private:
-	ETextureChannelType			m_format;
-	ID3D11Texture2D*			m_texture;
-	ID3D11ShaderResourceView*	m_shaderView;
+	ETextureChannelType					m_format;
+	ComPtr<ID3D11Texture2D>				m_texture;
+	ComPtr<ID3D11ShaderResourceView>	m_shaderView;
 
 public:
 	RTexture();
 	RTexture(ETextureChannelType format);
 	~RTexture();
+
+	bool StandardSetup(unsigned char* image, const UINT& width, const UINT& height);
+	bool GenerateMipMaps(unsigned char* image, const UINT& width, const UINT& height);
 
 	// Inherited via GResource
 	virtual bool Create(const std::string& filename) override;
