6538a6ea61ece2a75e4db188eeec6e4239117c07
Martin Höglund <martin.hoglund01@gmail.com>
2021-11-01 13:07:22 +0100

    Skybox pass

---
+++ Engine/src/SkyboxPass.cpp
@@ -0,0 +1,33 @@
+#include "SkyboxPass.h"
+#include "PipelineManager.h"
+
+void SkyboxPass::PreRender(Camera* pCam = nullptr, ID3D11DeviceContext* pDeviceContext = D3D11Core::Get().DeviceContext())
+{
+    //DC->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    //DC->IASetInputLayout(PM->m_defaultInputLayout.Get());
+
+    //DC->VSSetShader(PM->m_defaultVertexShader.Get(), nullptr, 0);
+    //DC->PSSetShader(PM->m_defaultPixelShader.Get(), nullptr, 0);
+
+    //DC->VSSetConstantBuffers(1, 1, pCam->m_viewConstantBuffer.GetAddressOf());
+
+    ////DC->PSSetShaderResources(0, 1, PM->m_depthBufferSRV.GetAddressOf());   // DepthBuffer.
+
+    //DC->PSSetSamplers(0, 1, PM->m_linearSamplerState.GetAddressOf());
+    //DC->PSSetSamplers(1, 1, PM->m_pointSamplerState.GetAddressOf());
+    //m_lights->Render(DC);
+
+    //DC->RSSetViewports(1, &PM->m_viewport);
+    //DC->RSSetState(PM->m_rasterState.Get());
+
+    //DC->OMSetRenderTargets(1, PM->m_backBuffer.GetAddressOf(), PM->m_depthStencilView.Get());
+    //DC->OMSetDepthStencilState(PM->m_depthStencilStateLessEqual.Get(), 0);
+}
+
+void SkyboxPass::Render(Scene* pScene)
+{
+}
+
+void SkyboxPass::PostRender(ID3D11DeviceContext* pDeviceContext = D3D11Core::Get().DeviceContext())
+{
+}
