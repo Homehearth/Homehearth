1231a6cbe9591a20ae6639695ab9dcdb45394516
Nikki <Nikki.Norberg@hotmail.com>
2021-11-19 15:40:47 +0100

    d

--- engine/src/TextureEffectPass.h
+++ engine/src/TextureEffectPass.h
@@ -9,19 +9,6 @@ class TextureEffectPass : public IRenderPass
 {
 private:
 
-	std::shared_ptr<RModel> m_WaterModel;
-	std::shared_ptr<RModel> m_WaterEdgeModel;
-	std::shared_ptr<RModel> m_WaterFloorModel;
-
-	std::vector<sm::Vector2> m_WaterUV;
-	std::vector<sm::Vector2> m_WaterEdgeUV;
-	std::vector<sm::Vector2> m_WaterFloorUV;
-
-	std::shared_ptr<RTexture> m_WaterAlbedoMap;
-	std::shared_ptr<RTexture> m_WaterNormalMap;
-	std::shared_ptr<RTexture> m_WaterEdgeAlbedoMap;
-	std::shared_ptr<RTexture> m_WaterFloorAlbedoMap;
-
 	const int m_MAX_PIXELS = 256 * 256;
 	float m_clearColor[4] = { 0.f,0.f,0.f,0.f };
 	texture_effect_t m_CBuffer;
@@ -36,6 +23,5 @@ public:
 
 	void PostRender(ID3D11DeviceContext* pDeviceContext = D3D11Core::Get().DeviceContext()) override;
 
-	void SetResources();
 };
 
