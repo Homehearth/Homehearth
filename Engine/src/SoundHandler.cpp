be98d9d24ffb04c11827b098f734caf48fecf1cc
Jonathan <53872055+Jonathan-source@users.noreply.github.com>
2021-11-29 15:29:31 +0100

    tuning values

--- Engine/src/SoundHandler.cpp
+++ Engine/src/SoundHandler.cpp
@@ -141,9 +141,9 @@ irrklang::ISound* SoundHandler::Play3DSound(const std::string& name, const sm::V
     m_iterator = m_soundSources.find(name);
     if (m_iterator != m_soundSources.end())
     {
-        sound = m_soundEngine->play3D(m_soundSources[name],{ pos.x, pos.y, pos.z },
+        sound = m_soundEngine->play3D(m_soundSources[name],{ -pos.x, pos.y, pos.z },
             false, true, false, false);
-        sound->setMinDistance(300.f);
+        sound->setMinDistance(100.f);
         sound->setIsPaused(!playSound);
     }
 
