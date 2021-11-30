7e57b2e556ab7dcb211682b42079d270f9057ccc
Martin Höglund <martin.hoglund01@gmail.com>
2021-11-30 17:53:32 +0100

    Decals are back!

--- Engine/src/Renderer.cpp
+++ Engine/src/Renderer.cpp
@@ -16,10 +16,12 @@ void Renderer::Initialize(Window* pWindow)
 		Had to disable the depth pass to get alpha testing to work correctly... -Filip
 	*/
 	//AddPass(&m_depthPass);  // 1
-	AddPass(&m_decalPass);
-	m_decalPass.Create();
 	AddPass(&m_shadowPass);
 	m_shadowPass.StartUp();
+
+	AddPass(&m_decalPass);
+	m_decalPass.Create();
+
 	AddPass(&m_basePass);   // 2
 	AddPass(&m_animPass);	// 3
 	AddPass(&m_skyPass);
