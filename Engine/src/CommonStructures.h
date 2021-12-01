e4d5eb8cb60a271d7a0037925aebda341670be80
Julius <julius.wendt@live.se>
2021-12-01 20:31:47 +0100

    Skybox tint improve

--- Engine/src/CommonStructures.h
+++ Engine/src/CommonStructures.h
@@ -16,14 +16,18 @@ constexpr float ROTATION = 180.0f / (float)(TIME_LIMIT_DAY + TIME_LIMIT_MORNING)
 const float DAY_DURATION	= 150.0f;
 const float MORNING			= 0.0f;
 const float DAY				= 0.1f;
+const float MID_DAY			= 0.25f;
+const float EVENING			= 0.4f;
 const float NIGHT			= 0.5f;
+const float EARLY_MORNING	= 0.9f;
 
 
 enum class CyclePeriod : UINT
 {
-	DAY,
-	NIGHT,
-	MORNING,
+	DAY,		// 0
+	NIGHT,		// 1
+	MORNING,	// 2
+	EVENING,	// 3
 };
 
 struct Currency
