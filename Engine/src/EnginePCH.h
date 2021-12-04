9f77df452681a03a70c12dcebc8c16fa88609c38
Rikard 'klanton' Lundberg <rikard.lundberg@hotmail.com>
2021-12-04 19:39:15 +0100

    Update EnginePCH.h

--- Engine/src/EnginePCH.h
+++ Engine/src/EnginePCH.h
@@ -1,7 +1,7 @@
 #pragma once
 
 //Options
-#define RENDER_IMGUI 1
+#define RENDER_IMGUI 0
 #define PROFILER 1
 
 // Feel free to add more things to this, currently only gives more money :)
@@ -11,7 +11,7 @@
 
 // Turn this to 1 if you want to see the very beautiful demo menu
 #define DRAW_TEMP_2D 0
-#define DEBUG_SNAPSHOT 1
+#define DEBUG_SNAPSHOT 0
 
 #define RENDER_GRID 0
 
